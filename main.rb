8e60eb42a63b58b1f4dc3960a720b954e8a716d1
sindry <DanRubyDeveloper@gmail.com>
2017-09-14 17:38:19 +0600

    dealer logic

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
 require_relative 'player'
 
+new_round = true
+round_menu = 'Choose your action:', '1 - skip', '2 - add card', '3 - open cards'
+
 show_data = proc do |player, show_cards|
   puts "#{player.name}:"
   player.cards.each { |card| print card, ' ' } if show_cards
   player.cards.each { print Player::HIDDEN_CARD, ' ' } unless show_cards
   puts ''
-  puts "Money:#{player.money}, Points: #{player.points}"
+  puts "Money:#{player.money}, Points: #{player.points}" if show_cards
   puts ''
+end
+
+dealer_turn = proc do |dealer|
+  dealer.add_card if dealer.points < 10 && dealer.points < 19
 end
 
 # 1 init values, start game
@@ -14,16 +21,33 @@ loop do # new game
   puts 'Enter your name:'
   user = Player.new(gets.chomp)
   dealer = Player.new 'Dealer'
+  puts user, dealer
   loop do # next round initialize round data -10$ + 2 cards for both
-    user.prepare # if new round
-    dealer.prepare # if new round
-    show_data.call dealer, false #show data
+    system('clear')
+    user.prepare if new_round
+    dealer.prepare if new_round
+    show_data.call dealer, true #change to false
     show_data.call user, true
-
+    puts round_menu
+    command = gets.chomp
+    case command
+    when '1' # Dealer turn
+      new_round = false
+      dealer_turn.call dealer
+      round_menu = 'Choose your action:', '2 - add card', '3 - open cards'
+    when '2'
+      # user.add_card
+      # new_round = false
+    when '3'
+      round_menu = 'Choose your action:', '1 - skip', '2 - add card', '3 - open cards'
+      new_round = true
+      break if command == '3'
+    end
     # round end user.money = 0, user asked to open cards, user and dealer have 3 cards
-    break
   end
-  puts '1 - New game', '0 - Exit game'
+  puts 'Game Over' if user.money.zero?
+  puts 'Congratulations!!! VICTORY!!' if user.money > 100
+  puts '=====================', 'New game - press Enter', 'Exit game - enter 0'
   break if gets.chomp == '0'
 end
 
