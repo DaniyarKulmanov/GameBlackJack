93e37f4da4588940145b9b9ff6448d1bc890e467
sindry <DanRubyDeveloper@gmail.com>
2017-09-16 14:20:54 +0600

    player.rb - add_card/points, game.rb - random_card

---
+++ game.rb
@@ -0,0 +1,53 @@
+class Game
+  HIDDEN_CARD = "\u{1F0A0}".freeze
+  NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'D', 'E'].freeze
+  SUITS = %w[A B C D].freeze
+
+  attr_accessor :desk, :new_round
+
+  def initialize
+    @desk ||= []
+    @new_round = true
+    fill @desk
+  end
+
+  def fill(desk)
+    SUITS.each do |suit|
+      NUMBERS.each do |number|
+        convert_suit suit, number
+        card_point number
+        desk << @suit_number
+      end
+    end
+  end
+
+  def random_card
+    card = @desk[rand(0..@desk.size)]
+    @desk.delete card
+    card
+  end
+
+  private
+
+  attr_writer :suit_number
+
+  def convert_suit(suit, number)
+    card_code = "1F0#{suit}#{number}"
+    @suit_number = {}
+    @suit_number[:card] = ''
+    @suit_number[:card] << card_code.to_i(16)
+  end
+
+  def card_point(card_number)
+    case card_number.to_i
+    when (2..10)
+      @suit_number[:points] = card_number
+    when 0
+      @suit_number[:points] = 10
+    else
+      @suit_number[:points] = 1
+      @suit_number[:alter_points] = 11
+    end
+  end
+
+end
--- player.rb
+++ player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :cards, :points
   attr_accessor :money
 
   def initialize(name)
@@ -11,6 +11,14 @@ class Player
 
   def add_card(card_info)
     @cards << card_info[:card]
-    @points = card_info[:points] unless card_info[:alter_points]
+    puts card_info
+    add_points card_info[:points], card_info[:alter_points]
+  end
+
+  def add_points(points, alter_points)
+    sum = 0
+    sum = @points + alter_points unless alter_points.nil?
+    @points = sum if sum <= 21 && !alter_points.nil?
+    @points += points if alter_points.nil? || sum > 21
   end
 end
